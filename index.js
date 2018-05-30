4b1641cfc36d8c555193e08f3ec24309b7522595
ZhangYuan <zyct@vip.qq.com>
2018-05-30 12:42:35 +0800

    update

--- index.js
+++ index.js
@@ -6,20 +6,49 @@ class DynamicProgramming {
         this.singleResultList = [];
     }
 
-    getSingleResultList(getSingleResultFun) 
+    getSingleResultList(getSingleResultFunc) 
     {
         this.singleResultList = [];
         for (let itemKey=0;itemKey<this.itemList.length;itemKey++) 
         {
             this.singleResultList[itemKey] = [];
-            for(let nowPurpose=1;nowPurpose<=this.purpose;nowPurpose++)
+            for(let nowPurpose=0;nowPurpose<=this.purpose;nowPurpose++)
             {
-                this.singleResultList[itemKey][nowPurpose-1] = getSingleResultFun(
+                this.singleResultList[itemKey][nowPurpose] = getSingleResultFunc(
                     this.itemList[itemKey],nowPurpose
                 );
             }
         }
         return this.singleResultList;
+    }
+
+    findResult(findResultFunc)
+    {
+        let result = [];
+        for (let itemKey=this.itemList.length-1;itemKey>=0;itemKey--) 
+        {
+            for(let nowPurpose=this.purpose;nowPurpose>=0;nowPurpose--)
+            {
+                let isAgainRun = findResultFunc(
+                    this.itemList[itemKey],
+                    this.singleResultList[itemKey][nowPurpose],
+                    this.purpose,result
+                );
+                if (isAgainRun<0)
+                {
+                    if (isAgainRun==-1){return result;}
+                    if (isAgainRun==-2){break;}
+                    if (isAgainRun==-3){continue;}
+                }
+                
+            }
+        }
+    }
+
+    run(getSingleResultFunc,findResultFunc)
+    {
+        this.getSingleResultList(getSingleResultFunc);
+        return this.findResult(findResultFunc);
     }
 }
 
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const DynamicProgramming = require('./index');
+//change coin by dynamic programming
 class Coin
 {
     constructor(much)
@@ -7,10 +8,37 @@ class Coin
     }
 }
 let coinTypeList = [new Coin(1),new Coin(2),new Coin(5)];
-let dp = new DynamicProgramming(coinTypeList,13);
-// first make singleResultList
-let singleResultList = dp.getSingleResultList((item,nowPurpose)=>{
+let coinDP = new DynamicProgramming(coinTypeList,13);
+let totalMoney = 0;
+let coinResult = coinDP.run((item,nowPurpose)=>{
     return Math.ceil(nowPurpose/item.much);
+},(item,itemResult,purpose,result)=>{
+    let money = item.much*itemResult;
+    if (money<=purpose-totalMoney)
+    {
+        result.push({coin:item,num:itemResult});
+        if (totalMoney+money==purpose){return -1;}
+        totalMoney+=money;
+        return -2;
+    }
 });
-console.log(singleResultList)
-// second find least Need
+console.log(coinResult)
+
+//sort by dynamic programming
+let needSortList = [1,7,6,8,2,5,4,3];
+let sortDP = new DynamicProgramming(needSortList,needSortList.length-1);
+let countMap = {};
+let countIn = 0;
+let sortResult = sortDP.run((item,nowPurpose)=>{
+    if (countMap[item]==undefined){countMap[item]=0;}
+    if (item>needSortList[nowPurpose]){
+        countMap[item]++;
+    }
+    return countMap[item];
+},(item,itemResult,purpose,result)=>{
+    result[itemResult] = item;
+    countIn++;
+    if (countIn>purpose){return -1;} else {return -2;}
+});
+console.log(sortResult)
+
